4eb3e54dd5fe84764da208b5af9afb2e1aeaff5f
IhorPinchuk <pinchukkk@ukr.net>
2023-02-27 18:52:43 +0200

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryEl.addEventListener("click", handleImageClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class = "gallery__item">
+            <a class="gallery__link" href="#">
+                <img
+                    class = "gallery__image"
+                    src = "${preview}"
+                    data-source = "${original}"
+                    alt = "${description}">
+            </a>
+        </div>`;
+    })
+    .join("");
+}
+
+function handleImageClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.getAttribute("data-source")}">`
+  );
+
+  instance.show();
+
+  galleryEl.addEventListener("keydown", handleEscClose);
+
+  function handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      instance.close();
+    }
+  }
+}
