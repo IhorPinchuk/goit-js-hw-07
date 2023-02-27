687abe08fff2956d9f8a2165b0970f41a818e4e3
IhorPinchuk <pinchukkk@ukr.net>
2023-02-27 21:22:19 +0200

    task-1__2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,27 +3,81 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
+// -------------------------------- Варіант 1 --------------------------------
+
+// const galleryEl = document.querySelector(".gallery");
+// const galleryMarkup = createGalleryMarkup(galleryItems);
+// galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// galleryEl.addEventListener("click", handleImageClick);
+
+// function createGalleryMarkup(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+//         <div class = "gallery__item">
+//             <a class="gallery__link" href="#">
+//                 <img
+//                     class = "gallery__image"
+//                     src = "${preview}"
+//                     data-source = "${original}"
+//                     alt = "${description}">
+//             </a>
+//         </div>`;
+//     })
+//     .join("");
+// }
+
+// function handleImageClick(evt) {
+//   evt.preventDefault();
+//   if (!evt.target.classList.contains("gallery__image")) {
+//     return;
+//   }
+
+//   const instance = basicLightbox.create(
+//     `<img src="${evt.target.getAttribute("data-source")}">`
+//   );
+
+//   instance.show();
+
+//   galleryEl.addEventListener("keydown", handleEscClose);
+
+//   function handleEscClose(evt) {
+//     if (evt.key === "Escape") {
+//       instance.close();
+//     }
+//   }
+// }
+
+// -------------------------------- Варіант 2 --------------------------------
+
 const galleryEl = document.querySelector(".gallery");
 const galleryMarkup = createGalleryMarkup(galleryItems);
-galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
 
+galleryEl.append(...galleryMarkup);
 galleryEl.addEventListener("click", handleImageClick);
 
 function createGalleryMarkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-        <div class = "gallery__item">
-            <a class="gallery__link" href="#">
-                <img
-                    class = "gallery__image"
-                    src = "${preview}"
-                    data-source = "${original}"
-                    alt = "${description}">
-            </a>
-        </div>`;
-    })
-    .join("");
+  const items = [];
+  galleryItems.map((el) => {
+    const galleryItem = document.createElement("div");
+    galleryItem.className = "gallery__item";
+
+    const galleryLink = document.createElement("a");
+    galleryLink.className = "gallery__link";
+    galleryLink.href = "#";
+
+    const galleryImage = document.createElement("img");
+    galleryImage.className = "gallery__image";
+    galleryImage.src = el.preview;
+    galleryImage.setAttribute("data-source", el.original);
+    galleryImage.alt = el.description;
+
+    galleryItem.append(galleryLink);
+    galleryLink.append(galleryImage);
+    items.push(galleryItem);
+  });
+  return items;
 }
 
 function handleImageClick(evt) {
